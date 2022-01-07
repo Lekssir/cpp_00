1d51662ed6e2644e45287411c7570ce7404fde35
dweeper <isserq@gmail.com>
2022-01-08 00:54:19 +0300

    fix01

--- ex01/my_phone_book.hpp
+++ ex01/my_phone_book.hpp
@@ -6,7 +6,7 @@
 /*   By: dweeper <dweeper@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/28 14:43:47 by dweeper           #+#    #+#             */
-/*   Updated: 2022/01/05 20:55:35 by dweeper          ###   ########.fr       */
+/*   Updated: 2022/01/08 00:44:10 by dweeper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,16 +25,16 @@ class	Contact
 	std::string	darkest_secret;
 };
 
-class Phone_book
-{
-private:
-	Contact	cont_array[8];
+class	Phone_book {
 public:
 	Phone_book(/* args */);
 	~Phone_book();
 	void	add();
 	void	search();
 	void	exit();
+private:
+	Contact	cont_array[8];
+	int		top_index;
 };
 
 Phone_book::Phone_book(/* args */)
